ba460296a25131be52ca48ff5d2a01c407f62355
silviu.gherman <silviu.gherman@axsys.ro>
2023-09-18 17:20:27 +0300

    Packages and startup fixes

--- index.jsx
+++ index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Menu from "./components/Menu/index"
-import Star from "./components/Star"
 
 function App() {
   return (
-    <h1>Your components go here</h1>
+    <h1>Test</h1>
   )
 }
 
