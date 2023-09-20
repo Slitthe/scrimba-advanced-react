ecc55f8835c8122108fef77387b4962c2dd86738
silviu.gherman <silviu.gherman@axsys.ro>
2023-09-20 17:54:47 +0300

    Storybook build

---
+++ storybook-static/assets/Card.stories-d9be16f8.js
@@ -0,0 +1,9 @@
+import{c as p,j as r}from"./style-c07b68db.js";import{A as g}from"./index.esm-2af592d9.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";const u="_card_1vacr_1",_="_withIcon_1vacr_10",h="_icon_1vacr_13",e={card:u,withIcon:_,icon:h};function n({children:a,icon:s,iconBackgroundColor:c}){const m=p(e.card,s?e.withIcon:null);return r.jsxs("div",{className:m,children:[a,s&&r.jsx("div",{style:{backgroundColor:c},className:e.icon,children:s})]})}n.__docgenInfo={description:"",methods:[],displayName:"Card"};n.__docgenInfo={description:"",methods:[],displayName:"Card"};const v={component:n,render:({children:a,icon:s,iconBackgroundColor:c})=>r.jsx("div",{style:{marginTop:32},children:r.jsx(n,{icon:s?r.jsx(g,{}):null,iconBackgroundColor:c,children:r.jsx("div",{style:{fontWeight:400,color:"#6B7280",textAlign:"center",fontSize:16},children:a})})})},t={args:{children:"Content"}},o={...t,args:{...t.args,icon:!0,iconBackgroundColor:"#43a6b0"}};var i,d,l;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  ...baseArgs,
+  args: {
+    ...baseArgs.args,
+    icon: true,
+    iconBackgroundColor: "#43a6b0"
+  }
+}`,...(l=(d=o.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const b=["Card"];export{o as Card,b as __namedExportsOrder,v as default};
+//# sourceMappingURL=Card.stories-d9be16f8.js.map
